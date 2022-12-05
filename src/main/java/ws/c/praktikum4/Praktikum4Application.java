2207d4e92783e62f9ab1a74b3f5ddb6184f232b6
abidbe <abid.i.ft20@mail.umy.ac.id>
2022-12-05 20:10:57 +0700

    first commit

---
+++ src/main/java/ws/c/praktikum4/Praktikum4Application.java
@@ -0,0 +1,13 @@
+package ws.c.praktikum4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Praktikum4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Praktikum4Application.class, args);
+	}
+
+}
---
+++ src/test/java/ws/c/praktikum4/Praktikum4ApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.c.praktikum4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Praktikum4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
